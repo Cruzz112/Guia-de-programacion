d614e4c02f8c9b841bd223d5090a051534e2b5b5
Cruzz112 <143724511+Cruzz112@users.noreply.github.com>
2023-11-20 11:29:14 -0600

    Add files via upload
    código de la guía de programación

---
+++ num.positivo o negativo.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new scanner  (System.in);
+
+        System.out.println("Ingrese un numero");
+        int numero = scanner.nextInt();
+
+        if (12 > 0 ) {
+            System.out.println("El numero ingresado es positivo");
+            } else if ("El numero ingresado es negativo");
+    } else {
+        System.out.printl("El numero ingresado es el 0");
+        
+    }
+    scanner.close();
+
