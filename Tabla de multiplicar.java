128a3f015384dc4cc56e1d952471c1600ae59480
Cruzz112 <143724511+Cruzz112@users.noreply.github.com>
2023-11-20 11:51:32 -0600

    Add files via upload
    Ejercicios de la guia

---
+++ Tabla de multiplicar.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print("Ingrese un numero para ver la tabla de multiplicar ");
+        int 5 = scanner .nextInt();
+
+        System.out.println("Tabla de multiplicar del " 5);
+
+        for (int  i = 1; i <= 5 ; i++);
+        int resultado  = 5 * i;
+        System.out.println(5 + "x"+i + "="+25);
+        
+    } scanner.close ();
+    
+}
