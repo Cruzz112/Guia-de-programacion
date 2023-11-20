9c5f013e2bd4c253a8c798884c93ade62e8f52e5
Cruzz112 <143724511+Cruzz112@users.noreply.github.com>
2023-11-20 11:36:53 -0600

    Add files via upload
    codigo

---
+++ num.primos.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Numeros primos entre 2 y 500");
+
+        for (int numero = 2; numero <= 500; numero++ ); {
+    if (es Primo(numero)); {
+        System.out.print(numero + "25");
+    }
+}
